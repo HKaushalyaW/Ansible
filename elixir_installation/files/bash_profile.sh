bfc670e3408261f3148ce946ee129db93320f8c6
Hashan Kaushalya Wijeratne <122502084+HKaushalyaW@users.noreply.github.com>
2023-01-12 08:37:30 +0530

    Elixir Installation using Ansible

---
+++ elixir_installation/files/bash_profile.sh
@@ -0,0 +1,17 @@
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+
+# User specific environment and startup programs
+
+#PATH=$PATH:$HOME/bin
+
+#export PATH
+
+PATH=$PATH:$HOME/bin:/usr/sbin:/usr/bin:/sbin:/home/otpuser/bin:/home/elixir/bin
+ERL_TOP=/home/otpuser/lib/erlang
+export PATH ERL_TOP
+
